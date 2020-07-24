da89c0c4f7cb1c53b26f7759773408f6960ceac5
Muhammad Nadeem <devedeem@gmail.com>
2020-07-25 02:18:59 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+
+const laptopData = JSON.parse(json);
+// console.log(laptopData);
+
+const server = http.createServer((req, res) => {
+
+    const pathname = url.parse(req.url, true).pathname;
+    // console.log(pathname);
+
+    const id = url.parse(req.url, true).query.id;
+    // console.log(id);
+    
+    // PRODUCT OVERVIEW
+    if(pathname === '/products' || pathname === '/') {
+        
+    res.writeHead(200, { 'Content-type0' : 'text/html'});
+
+    fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+        let overviewOutput = data;
+        fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+            const cardsOutput = laptopData.map(el => replaceTmlate(data, el)).join('');
+            overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+            res.end(overviewOutput);
+        });
+    });
+
+
+    // LAPTOP DEATAIL
+    } else if (pathname === '/laptop' && id < laptopData.length) {
+
+        res.writeHead(200, { 'Content-type0' : 'text/html'});
+
+        
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+           const output = replaceTmlate(data, laptop);
+            res.end(output);
+        });
+    }
+
+    //IMAGES
+    else if ((/\.(jpeg|jpg|png|gif)$/i).test(pathname)){
+        fs.readFile(`${__dirname}/data/img${pathname}`,(err, data) => {
+            res.writeHead(200, { 'Content-type0' : 'image/jpg'});
+            res.end(data); 
+        });
+    }
+    // URL NOT FOUND
+    else {
+        res.writeHead(404, { 'Content-type0' : 'text/html'});
+    res.end('URL was not found on the server');
+    }
+
+});
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for request now');
+});
+
+function replaceTmlate(orignalHtml, laptop) {
+    let output = orignalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+
+    return output;
+}
